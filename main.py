5bf4ea5e085997f69671c865442eed6be8be0673
Shams M. Monem <shamsmmonem@gmail.com>
2024-04-30 12:47:41 +0300

    Video frames into byte array

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 from math import ceil
 
 from PIL import Image
+import cv2
 
-file_image = Image.open(
-    "image.png")
-input_image = file_image.resize((128, 64)).convert("RGB")
-width, height = input_image.size
+# Load the video file
+# Change the path to the video file as per your system
+video_file = "/home/shams/Videos/Webcam/2024-04-27-002825.webm"
+cap = cv2.VideoCapture(video_file)
 
-size = width * ceil((height / 8))
-output = bytearray(size)
-threshold = 100
+output = bytearray(5 * 128 * 8)
 
-for i in range(width):
-    for k in range(ceil(height / 8)):
-        index = i + k * width
-        output[index] = 0
-        for j in range(8):
-            pixel = input_image.getpixel((i, (k * 8) + j))
-            magnitude = sum(pixel) / 3
-            output[index] |= (magnitude > threshold) << j
+threshold = 60
+count = 0
 
+while True:
+    # Read a frame from the video
+    ret, frame = cap.read()
+    count += 1
 
-print("const unsigned char data[] = {{{}}};".format(", ".join([format(b, '#04x') for b in output])))
+    if count == 5:
+        break
+
+    # Check if frame is retrieved successfully
+    if not ret:
+        break
+
+    # Convert the frame to PIL Image format
+    pil_frame = Image.fromarray(frame)
+
+    # Resize and convert the frame to RGB
+    resized_frame = pil_frame.resize((128, 64)).convert("RGB")
+
+    for i in range(resized_frame.size[0]):
+        for k in range(ceil(resized_frame.size[1] / 8)):
+            index = i + k * resized_frame.size[0] + count * 1024
+            output[index] = 0
+            for j in range(8):
+                pixel = resized_frame.getpixel((i, (k * 8) + j))
+                magnitude = sum(pixel) / 3
+                output[index] |= (magnitude > threshold) << j
+
+# Release the video capture object
+cap.release()
+
+print("const unsigned char data[][1024] = {{{}}};".format(", ".join([format(b, '#04x') for b in output])))
